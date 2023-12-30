8d2fbde82ab71ea39653c3ccd3605a9b61013632
Farokh Madah <fa.madah@gmail.com>
2023-12-30 14:08:03 +0330

    vrc25pcusd sdk

---
+++ src/sdk/evm-v1.0/VRC25PCUSD.js
@@ -0,0 +1,23 @@
+const { ethers } = require('ethers')
+const contractAbi = require('@/contracts/abi/VRC25PCUSD')
+const contractDep = require(`@/contracts/deployments/${process.env.VUE_APP_DEFAULT_NETWORK}/VRC25PCUSD.json`)
+const contractAddress = contractDep.address
+
+class SDK {
+	constructor(_, signer) {
+		this.contract = new ethers.Contract(contractAddress, contractAbi, signer)
+	}
+
+	balanceOf(owner) {
+		return this.contract.balanceOf(owner)
+	}
+	
+	async transfer(recipient, amount) {
+		const tx = await this.contract.transfer(_to, 10000000, {
+			gasLimit: 4000000
+		});
+		return await tx.wait();
+	}
+}
+
+module.exports = SDK
